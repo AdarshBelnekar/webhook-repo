6484e6eda681ade04b857e11fae78e2b5e302d4c
AdarshBelnekar <adarshbelnekar123@gmail.com>
2025-07-04 18:47:58 +0530

    Added .env support for Mongo URI and updated configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from app.extensions import mongo
 from app.webhooks.routes import webhook_bp
+import os
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = "mongodb+srv://bussinessa144:webhookthepassword00@web.8nbzp3t.mongodb.net/?retryWrites=true&w=majority&appName=web"
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     mongo.init_app(app)
     app.register_blueprint(webhook_bp)
     return app
--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,5 @@
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 
+load_dotenv()
 mongo = PyMongo()
